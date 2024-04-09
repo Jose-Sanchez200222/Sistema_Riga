0884b22441adb47bcc96bf1be077c69b4b8caa00
Rigebs <riclarobr04@gmail.com>
2024-04-09 18:08:42 -0500

    add: Se añadieron controladores y modelos

--- sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/SistemaRigaBackendApplication.java
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/SistemaRigaBackendApplication.java
@@ -1,5 +1,6 @@
 package com.sistema.riga.sistema_riga_backend;
 
+import com.sistema.riga.sistema_riga_backend.models.DistritoModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class SistemaRigaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaRigaBackendApplication.class, args);
-	}
 
+		DistritoModel distritoModel = new DistritoModel();
+
+	}
 }
---
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/models/DistritoModel.java
@@ -0,0 +1,10 @@
+package com.sistema.riga.sistema_riga_backend.models;
+
+import lombok.Data;
+
+@Data
+public class DistritoModel {
+    long idDistrito;
+    String nombreDistrito;
+    long idProvincia;
+}
